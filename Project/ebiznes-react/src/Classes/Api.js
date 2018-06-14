3afcac069bf059536df6304048dfb3eb7ded83eb
JakubWidaEBiznes <jakub.wida.box@gmail.com>
2018-06-14 19:28:57 +0200

    added React REST tester

--- Project/ebiznes-react/src/Classes/Api.js
+++ Project/ebiznes-react/src/Classes/Api.js
@@ -1,3 +1,21 @@
 export class Api {
-constructor(){}
+	constructor(){}
+
+get(url){
+	return fetch(url,{method:"GET"}).then((response) => response.json())
+	}
+post(url,message){
+	return fetch(url,{method:"POST",body:JSON.stringify(message)}).then((response) => response.json())
+	}
 }
+
+/*
+fetch('https://facebook.github.io/react-native/movies.json')
+	.then((response) => response.json())
+	.then((responseJson) => {
+		return responseJson.movies;
+	})
+	.catch((error) => {
+		console.error(error);
+	});
+*/
--- Project/ebiznes-react/src/Main/Router/RestTester/RestTester.js
+++ Project/ebiznes-react/src/Main/Router/RestTester/RestTester.js
@@ -1,27 +1,78 @@
 import React, { Component } from 'react';
 
-class RestTester extends Component {
-	constructor(props)
-		{
+export default class RestTester extends Component {
+	constructor(props){
 		super(props);
-		this.state = {id:props.auth.user.id}
+		this.state = {
+			id:props.auth.user.id,
+			targetUrl:"",
+			message:'{ "data":"0" }',
+			method:"GET",
+			response:"",
+			error:""
+			}
+		this.handleInputChange = this.handleInputChange.bind(this);
 		}
   render() {
     return (
       <div>
 				<p>REst tester</p>
 				<p>{this.state.id}</p>
+				<input type="text" name="targetUrl" placeholder="url" onChange={this.handleInputChange}/>
+				<input type="text" name="message" placeholder="{count:0}" onChange={this.handleInputChange}/>
+				<select name="method" onChange={this.handleInputChange}>
+					<option value="GET">GET</option>
+					<option value="POST">POST</option>
+				</select>
+				<button onClick={()=>{this.tryRest()}}>TryRest</button>
+				{this.state.response && <p>this.state.response</p>}
+
       </div>
     );
   }
+handleInputChange(e){
+	this.setState({[e.target.name]: e.target.value });
+	}
+tryRest(){
+	/*
+	let self = this;
+	console.log("trying rest",
+	self.state.method,
+	self.state.targetUrl,
+	JSON.parse(self.state.message));
+	*/
+	var self = this;
+	if(this.state.method=="POST"){
+		this.props.api.post(this.state.targetUrl,JSON.parse(self.state.message))
+			.then((response)=>{this.onRestResponse(response)})
+			.catch((err)=>{this.onRestError(err)})
+		}
+	else if(this.state.method=="GET"){
+		this.props.api.get(this.state.targetUrl,JSON.parse(self.state.message))
+			.then((response)=>{this.onRestResponse(response)})
+			.catch((err)=>{this.onRestError(err)})
+		}
+	}
+
+onRestResponse(data){
+	console.log("onResponse:",data);
+	this.setState({response:data});
+	}
+
+onRestError(data){
+	console.log("onError:",data);
+	this.setState({error:data});
+	}
 }
 
-export default RestTester;
+/*
 
-fetch('http://example.com/movies.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  });
+fetch('https://facebook.github.io/react-native/movies.json')
+	.then((response) => response.json())
+	.then((responseJson) => {
+		return responseJson.movies;
+	})
+	.catch((error) => {
+		console.error(error);
+	});
+*/
